412de26b46be967e407b8cbb05e3b5b6ec1c9eff
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2021-12-13 18:44:50 +0100

    Generate one disaster article

--- assets/js/display.js
+++ assets/js/display.js
@@ -4,8 +4,19 @@ function renderDisasters(???) {
 
 }
 
-function renderBasicInfo(disaster){
-
+function renderBasicInfo(disaster, category, level){
+    return `<article>
+        <h3>${disaster}</h3>
+        <figure>
+            <img src="images/flood.svg" alt="${disaster}" title="${disaster}">
+        </figure>
+        <dl>
+            <dt>Category:</dt>
+            <dd>${category}</dd>
+            <dt>Level</dt>
+            <dd>${level}</dd>
+        </dl>
+    </article>`
 }
 
 function renderAid(disaster){
