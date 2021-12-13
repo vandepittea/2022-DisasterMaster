5a54176ef96bc64bda6e2ef2abf9c558993754e7
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2021-12-13 15:23:06 +0100

    Show country list under search bar

--- assets/js/log-disaster/log-disaster.js
+++ assets/js/log-disaster/log-disaster.js
@@ -1,7 +1,18 @@
 "use strict";
 
 function suggestCountry(e){
+    /* search */
 
+    fillCountriesList(countries, "#disaster-location");
+}
+
+function fillCountriesList(countries, id){
+    const $ul = document.querySelector(`${id} ul`);
+    $ul.innerHTML = "";
+
+    for(const country of countries){
+        $ul.insertAdjacentHTML('beforeend', `<li>${country}</li>`);
+    }
 }
 
 function selectCountry(e){
