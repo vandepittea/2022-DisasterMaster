99d5208cfc16b1b9a0ff4c508e3f8096d0fb132c
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2021-12-14 20:16:13 +0100

    Execute a function when clicking on an aid

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ function init() {
     document.querySelector("#disaster-location input").addEventListener('input', suggestCountry);
     document.querySelector("#disaster-location ul").addEventListener('click', selectCountry);
     document.querySelector(".disasters").addEventListener('click', selectDisaster);
+    document.querySelector(".aids").addEventListener('click', selectAid);
 
     const buttons = document.querySelectorAll("button")
     buttons.forEach((button) => {
