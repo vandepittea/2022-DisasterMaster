332be9280c3bfd9beb03950b6b2638ab137cb705
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2021-12-15 22:14:52 +0100

    Fix error errors in submitDisaster

--- assets/js/log-disaster/submit-disaster.js
+++ assets/js/log-disaster/submit-disaster.js
@@ -3,25 +3,30 @@
 function submitDisaster(e){
     e.preventDefault();
 
-    let doubleDisasterInSameCountry = false;
-    let submittedDisastersLocalStorage = loadFromStorage(config.submittedDisastersKey);
+    if(selectedAid !== undefined){ // global variable
+        let doubleDisasterInSameCountry = false;
+        const disasterObject = selectDisasterObject();
+        const disaster = saveDisaster(disasterObject, selectedCountry, selectedAid, determineAid(showAid(e), aid), determineCurrency(disasterObject, selectedCountry))
+        let submittedDisastersLocalStorage = loadFromStorage(config.submittedDisastersKey);
 
-    if (submittedDisastersLocalStorage == null){
-        submittedDisastersLocalStorage = [];
-    }
-
-    for(const submittedDisaster of submittedDisastersLocalStorage){
-        if(submittedDisaster.name === disaster.name && submittedDisaster.location === disaster.location){
-            doubleDisasterInSameCountry = true;
+        if (submittedDisastersLocalStorage == null){
+            submittedDisastersLocalStorage = [];
         }
-    }
 
-    if(doubleDisasterInSameCountry === false){
-        submittedDisastersLocalStorage.push(disaster);
-        saveToStorage(config.submittedDisastersKey, submittedDisastersLocalStorage);
-    }
-    else{
-        console.log("ERROR: double disaster in same country");
+        for(const submittedDisaster of submittedDisastersLocalStorage){
+            if(submittedDisaster.name === disaster.name && submittedDisaster.location === disaster.location){
+                doubleDisasterInSameCountry = true;
+            }
+        }
+
+        if(doubleDisasterInSameCountry === false){
+            submittedDisastersLocalStorage.push(disaster);
+            saveToStorage(config.submittedDisastersKey, submittedDisastersLocalStorage);
+        }
+        else{
+            console.log("ERROR: double disaster in same country");
+            console.log(selectedAid);
+        }
     }
 }
 
