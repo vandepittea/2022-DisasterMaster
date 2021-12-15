8010e19952cbb7952a0ea471ef76b03fe8b154b4
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2021-12-15 22:15:29 +0100

    Execute function when the submit button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,18 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    document.querySelector("#disaster-location input").addEventListener('input', suggestCountry);
-    document.querySelector("#disaster-location ul").addEventListener('click', selectCountry);
-    document.querySelector(".disasters").addEventListener('click', selectDisaster);
-    document.querySelector(".aids").addEventListener('click', selectAid);
-    document.querySelector("#show-aid").addEventListener('click', showAid);
-    document.querySelector("#disaster-location button").addEventListener('click', showDisasters);
-
     const buttons = document.querySelectorAll("button")
     buttons.forEach((button) => {
         button.addEventListener('click', navigate);
     });
+
+    document.querySelector("#disaster-location input").addEventListener('input', suggestCountry);
+    document.querySelector("#disaster-location ul").addEventListener('click', selectCountry);
+    document.querySelector(".disasters").addEventListener('click', selectDisaster);
+    document.querySelector("#show-aid").addEventListener('click', showAid);
+    document.querySelector(".aids").addEventListener('click', selectAid);
+    document.querySelector("#disaster-location button").addEventListener('click', showDisasters);
+    document.querySelector("button[type='submit']").addEventListener('click', submitDisaster);
 }
 
 function initSubmitDisaster(){
