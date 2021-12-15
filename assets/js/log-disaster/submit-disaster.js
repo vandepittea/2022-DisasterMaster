052d31540e5644a9e94b375563335cade3f9c435
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2021-12-15 15:13:38 +0100

    Return a new object

--- assets/js/log-disaster/submit-disaster.js
+++ assets/js/log-disaster/submit-disaster.js
@@ -1,7 +1,17 @@
 "use strict";
 
-function submitDisaster(e){
-
+function submitDisaster(e, disaster, countryName, aidName, aidGoal, currencyGoal){
+    return {
+        "name": disaster.name,
+        "category": disaster.category,
+        "level": disaster.level,
+        "location": countryName,
+        "aidProgress": 0,
+        "currencyProgress": 0,
+        "aidGoal": aidGoal,
+        "currencyGoal": currencyGoal,
+        "requestedAid": aidName
+    }
 }
 
 function saveDisaster(disaster){
