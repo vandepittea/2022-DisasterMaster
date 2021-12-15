a6fc8534a89297e0d157977cbe2b883ea33e9d02
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2021-12-15 21:14:22 +0100

    Swap submitDisaster and saveDisaster

--- assets/js/log-disaster/submit-disaster.js
+++ assets/js/log-disaster/submit-disaster.js
@@ -1,20 +1,8 @@
 "use strict";
 
-function submitDisaster(e, disaster, countryName, aidName, aidGoal, currencyGoal){
-    return {
-        "name": disaster.name,
-        "category": disaster.category,
-        "level": disaster.level,
-        "location": countryName,
-        "aidProgress": 0,
-        "currencyProgress": 0,
-        "aidGoal": aidGoal,
-        "currencyGoal": currencyGoal,
-        "requestedAid": aidName
-    }
-}
+function submitDisaster(e){
+    e.preventDefault();
 
-function saveDisaster(disaster){
     let doubleDisasterInSameCountry = false;
     let submittedDisastersLocalStorage = loadFromStorage(config.submittedDisastersKey);
 
@@ -34,6 +22,20 @@ function saveDisaster(disaster){
     }
     else{
         console.log("ERROR: double disaster in same country");
+    }
+}
+
+function saveDisaster(disaster, countryName, aidName, aidGoal, currencyGoal){
+    return {
+        "name": disaster.name,
+        "category": disaster.category,
+        "level": disaster.level,
+        "location": countryName,
+        "aidProgress": 0,
+        "currencyProgress": 0,
+        "aidGoal": aidGoal,
+        "currencyGoal": currencyGoal,
+        "requestedAid": aidName
     }
 }
 
