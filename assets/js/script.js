8bdef9d18ab94761d5bad7797231f0798cc420fe
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2021-12-16 15:03:30 +0100

    Calling functions in initSubmitDisaster and initSubmitDisaster and calling initSupportDisasters in init

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,23 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
+        initSubmitDisaster();
+        initSupportDisasters();
+}
+
+function initSubmitDisaster(){
+    document.querySelector("#disaster-location input").addEventListener('input', suggestCountry);
+    document.querySelector("#disaster-location ul").addEventListener('click', selectCountry);
+    document.querySelector("#disaster-location button").addEventListener('click', showDisasters);
+    document.querySelector(".disasters").addEventListener('click', selectDisaster);
+    document.querySelector("#show-aid").addEventListener('click', showAid);
+    document.querySelector(".aids").addEventListener('click', selectAid);
+    document.querySelector("button[type='submit']").addEventListener('click', submitDisaster);
+
     const buttons = document.querySelectorAll("button")
     buttons.forEach((button) => {
         button.addEventListener('click', navigate);
     });
-
-    document.querySelector("#disaster-location input").addEventListener('input', suggestCountry);
-    document.querySelector("#disaster-location ul").addEventListener('click', selectCountry);
-    document.querySelector(".disasters").addEventListener('click', selectDisaster);
-    document.querySelector("#show-aid").addEventListener('click', showAid);
-    document.querySelector(".aids").addEventListener('click', selectAid);
-    document.querySelector("#disaster-location button").addEventListener('click', showDisasters);
-    document.querySelector("button[type='submit']").addEventListener('click', submitDisaster);
-}
-
-function initSubmitDisaster(){
-
 }
 
 function initSupportDisasters() {
