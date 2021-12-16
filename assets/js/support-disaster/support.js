446310888d3c4ad616cbf3fe2fc394a0d119f2de
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2021-12-16 16:17:26 +0100

    Load the supported disasters

--- assets/js/support-disaster/support.js
+++ assets/js/support-disaster/support.js
@@ -9,3 +9,8 @@ function supportDisaster(e) {
 }
 
 // Add additional functions below
+function showSupportableDisasters(e){
+    e.preventDefault();
+
+    renderDisasters("#submitted-disasters div", loadFromStorage(config.submittedDisastersKey));
+}
